8b2edb8715b7643b706630cfc814186b3a3ba713
Julian Poyourow <julianpoyo@gmail.com>
2016-11-06 13:19:34 -0800

    Added fish aliases

---
+++ init.sh
@@ -0,0 +1,24 @@
+#!/usr/local/bin/fish
+
+alias gpo "git push origin"
+funcsave gpo
+alias gp "git pull"
+funcsave gp
+alias gf "git fetch"
+funcsave gf
+alias gc "git commit"
+funcsave gc
+alias gca "git add .; and git commit"
+funcsave gca
+alias dongrok "cd ~/Projects; and ./ngrok start jnode jangular jionic"
+funcsave dongrok
+alias cpu "watch grep \"cpu MHz\" /proc/cpuinfo"
+funcsave cpu
+alias gs "git status"
+funcsave gs
+alias ga "git add"
+funcsave ga
+alias gaa "git add ."
+funcsave gaa
+alias gcb "git checkout -b"
+funcsave gcb
