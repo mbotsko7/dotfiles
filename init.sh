38e4a0215fdaf774facaf54115c76acad01f4acf
Julian Poyourow <julianpoyo@gmail.com>
2017-02-05 19:20:32 -0800

    Added governor

--- init.sh
+++ init.sh
@@ -32,3 +32,5 @@ alias kernel='uname -r'
 funcsave kernel
 alias cpu='watch -n 0.4 grep \"cpu MHz\" /proc/cpuinfo'
 funcsave cpu
+alias governor='cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor'
+funcsave governor
