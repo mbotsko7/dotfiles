c29d868f2f6b637a0b14db399b285627c38b538e
Julian Poyourow <julianpoyo@gmail.com>
2017-06-30 12:50:02 -0700

    Split off git into separate file

---
+++ git.sh
@@ -0,0 +1,26 @@
+# Git specific
+alias gpo "git push origin"
+funcsave gpo
+alias gpoh "git push origin HEAD"
+funcsave gpoh
+alias gp "git pull origin HEAD"
+funcsave gp
+alias gf "git fetch --all"
+funcsave gf
+function setupstream
+git branch --set-upstream-to=origin/(git rev-parse --abbrev-ref HEAD) (git rev-parse --abbrev-ref HEAD)
+end
+funcsave setupstream
+alias gc "git commit"
+funcsave gc
+alias gca "git add .; and git commit"
+funcsave gca
+alias gs "git status"
+funcsave gs
+alias ga "git add"
+funcsave ga
+alias gaa "git add ."
+funcsave gaa
+alias gcb "git checkout"
+funcsave gcb
+
