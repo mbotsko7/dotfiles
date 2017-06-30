35ad2d8d0bc0fe1395f3205b5be2dd083e263ba8
Julian Poyourow <julianpoyo@gmail.com>
2017-06-30 13:17:22 -0700

    Added lock alias

--- i3.sh
+++ i3.sh
@@ -3,3 +3,5 @@ alias chrome "google-chrome-desktop &"
 funcsave chrome
 alias gmusic "google-play-music-desktop-player &"
 funcsave gmusic
+alias lock "i3lock -i ~/Downloads/forest.png"
+funcsave lock
