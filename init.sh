33fa4efec0501da69cb7ba6f31b02f93a3deeadd
Julian Poyourow <julianpoyo@gmail.com>
2017-06-30 12:24:37 -0700

    Organized aliases

--- init.sh
+++ init.sh
@@ -1,3 +1,8 @@
+# Application launch aliases
+alias chrome "google-chrome-desktop &"
+alias gmusic "google-play-music-desktop-player &"
+
+# Git specific
 alias gpo "git push origin"
 funcsave gpo
 alias gpoh "git push origin HEAD"
@@ -14,10 +19,6 @@ alias gc "git commit"
 funcsave gc
 alias gca "git add .; and git commit"
 funcsave gca
-alias dongrok "cd ~/Projects; and ./ngrok start jnode jangular jionic"
-funcsave dongrok
-alias cpu "watch grep \"cpu MHz\" /proc/cpuinfo"
-funcsave cpu
 alias gs "git status"
 funcsave gs
 alias ga "git add"
@@ -26,16 +27,22 @@ alias gaa "git add ."
 funcsave gaa
 alias gcb "git checkout"
 funcsave gcb
+
+# Copy paste tools
 alias pbcopy='xsel --clipboard --input'
 funcsave pbcopy
 alias pbpaste='xsel --clipboard --output'
 funcsave pbpaste
+
+# Linux specific monitoring
 alias kernel='uname -r'
 funcsave kernel
 alias cpu='watch -n 0.4 grep \"cpu MHz\" /proc/cpuinfo'
 funcsave cpu
 alias governor='cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor'
 funcsave governor
+
+# Linux specific debugging
 alias governor-set="sudo cpufreq-set -c 0 -g"
 funcsave governor-set
 alias wifi="sudo service network-manager restart"
