15a77d1660ce6912349d717cbce697f2f95ddf9a
Julian Poyourow <julianpoyo@gmail.com>
2018-01-16 05:02:19 +0000

    Switched to PPA instead of NVM (was a pain)

--- box-setup.sh
+++ box-setup.sh
@@ -4,23 +4,12 @@
 omf install bobthefish
 omf install foreign-env
 
-sudo apt-get update
+# Install node 8 via PPA
+curl -sL https://deb.nodesource.com/setup_8.x -o nodesource_setup.sh
+sudo bash nodesource_setup.sh
+rm nodesource_setup.sh
 
-# Install node via NVM
-sudo apt-get install build-essential libssl-dev
-curl -sL https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh -o install_nvm.sh
-bash ./install_nvm.sh
-source ~/.profile
-nvm install v8.9.4
-nvm use v8.9.4
-node -v
-nvm alias default v8.9.4
-nvm use default
-rm ./install_nvm.sh
-
-# Fix NVM fish
-alias nvm="fenv source ~/.nvm/nvm.sh \; nvm $argv"
-funcsave nvm
+sudo apt-get install nodejs build-essential
 
 # Install slap
 sudo npm install -g slap@latest
