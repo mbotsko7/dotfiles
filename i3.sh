7244ccef49c5f048ba52f0e0c6ac92b409ec3114
Julian Poyourow <julianpoyo@gmail.com>
2020-05-17 09:44:08 -0700

    Delete i3.sh

--- i3.sh
+++
@@ -1,7 +0,0 @@
-# Application launch aliases
-alias chrome "google-chrome-desktop &"
-funcsave chrome
-alias gmusic "google-play-music-desktop-player &"
-funcsave gmusic
-alias lock "i3lock -i ~/Downloads/forest.png"
-funcsave lock
