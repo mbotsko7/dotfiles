8f517b3ab7696f2674da61b55b920953f8c19aea
Julian Poyourow <julian@poyourow.com>
2023-03-01 06:37:42 +0000

    use v18 for default nvm

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ fish -c "fisher install oh-my-fish/bobthefish"
 
 # NVM
 fish -c "fisher install jorgebucaran/nvm.fish"
-fish -c "nvm install 16"
+fish -c "nvm install 18"
 fish -c "set --universal nvm_default_version 18"
 
 # NeoVim
